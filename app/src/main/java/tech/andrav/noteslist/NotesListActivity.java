00355bcb5fd097b666530379267d0f0dbcfda85e
andrav24 <andrav24@yandex.ru>
2019-12-01 20:56:18 +0700

    Setting Up a RecyclerView

--- app/src/main/java/tech/andrav/noteslist/NotesListActivity.java
+++ app/src/main/java/tech/andrav/noteslist/NotesListActivity.java
@@ -1,23 +1,54 @@
 package tech.andrav.noteslist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
+import tech.andrav.noteslist.adapters.NotesRecyclerAdapter;
 import tech.andrav.noteslist.models.Note;
 
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesListActivity";
 
+    // Ui components
+    private RecyclerView mRecyclerView;
+
+    // vars
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+        mRecyclerView = findViewById(R.id.recyclerView);
 
-        Note note = new Note("some title", "some content", "timestamp");
+        initRecyclerView();
+        insertFakeNotes();
+    }
 
-        Log.d(TAG, "onCreate: my note: " + note.toString());
+    private void insertFakeNotes(){
+        for(int i = 0; i < 1000; i++){
+            Note note = new Note();
+            note.setTitle("title #" + i);
+            note.setContent("content #: " + i);
+            note.setTimestamp("Dec 2019");
+            mNotes.add(note);
+        }
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNotesRecyclerAdapter);
     }
 }
---
+++ app/src/main/java/tech/andrav/noteslist/adapters/NotesRecyclerAdapter.java
@@ -0,0 +1,55 @@
+package tech.andrav.noteslist.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import tech.andrav.noteslist.R;
+import tech.andrav.noteslist.models.Note;
+
+public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<Note> mNotes;
+
+    public NotesRecyclerAdapter(ArrayList<Note> mNotes) {
+        this.mNotes = mNotes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_note_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
+        holder.timestamp.setText(mNotes.get(position).getTimestamp());
+        holder.title.setText(mNotes.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView timestamp, title;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            timestamp = itemView.findViewById(R.id.note_timestamp);
+            title = itemView.findViewById(R.id.note_title);
+
+        }
+    }
+
+}
